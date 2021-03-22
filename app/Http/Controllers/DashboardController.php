9dde6c41af7a5cac5c680484f047ba819dfc6535
Dmitry Matiouchenko <dmitrymatio@gmail.com>
2021-03-22 12:15:18 -0700

    best best

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,9 @@ use App\Mail\PostLiked;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Models\Note;
+use App\Models\Todo;
+use App\Models\Website;
+use App\Models\Image;
 
 class DashboardController extends Controller
 {
@@ -17,18 +20,43 @@ class DashboardController extends Controller
 
     public function index()
     {
-        return view('dashboard');
+        $notes = Note::where('user_id', auth()->user()->id)->orderBy('id', 'DESC')->first();
+        $todos = Todo::where('user_id', auth()->user()->id)->orderBy('id', 'DESC')->first();
+        $websites = Website::where('user_id', auth()->user()->id)->orderBy('id', 'DESC')->get();
+        $images= Image::where('user_id', auth()->user()->id)->orderBy('id', 'DESC')->paginate(4);;
+        return view('dashboard',[
+            'notes' => $notes,
+            'todos' => $todos,
+            'websites' => $websites,
+            'images' => $images
+        ]);
     }
 
     public function store(Request $request)
     {
-        $this->validate($request, [
-            'notes' => 'required',
-            'todos' => 'required'
-        ]);
 
-        $request->user()->notes()->create($request->only('notes'));
-        $request->user()->todos()->create($request->only('todos'));
+        if ($request->filled('notes'))  {
+            $request->user()->notes()->create($request->only('notes'));
+        }
+
+        if ($request->filled('todos')) {
+            $request->user()->todos()->create($request->only('todos'));
+        }
+
+        if ($request->filled('website')) {
+            $request->validate(['website'=>'required|url']);
+            $request->user()->websites()->create($request->only('website'));
+        }
+
+        if ($request->filled('images')) {
+            $request->validate(['images'=>'mimes:jpeg,jpg,png,gif|required']);
+
+            $file = $request->only(images);
+            $file->move('uploads', $file->getClientOriginalName());
+
+
+            $request->user()->images()->create('uploads/'.$file->getClientOriginalName());
+        }
 
         return back();
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,42 +17,24 @@
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-images">
                         Images
                     </label>
-                    <div class="flex justify-between space-x-5 mb-5">
+                    <div class="flex flex-col justify-between space-x-5 mb-5">
                         <input
-                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 @error('images') border-red-500 @enderror"
                             id="grid-images" type="file" name="images">
 
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-12">
-                            Add
-                        </button>
+                        @error('images')
+                            <div class="text-red-500 mt-2 text-sm">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <ul class="flex justify-between space-x-2">
-                        <li>
-                            <button
-                                class="w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                                google.com
-                            </button>
-                        </li>
-                        <li>
-                            <button
-                                class="w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                                google.com
-                            </button>
-                        </li>
-                        <li>
-                            <button
-                                class="w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                                google.com
-                            </button>
-                        </li>
-                        <li>
-                            <button
-                                class="w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                                google.com
-                            </button>
-                        </li>
+                        @foreach ($images as $image)
+                            <li>
+                                <img src="{{ $image->image }}" alt="">
+                            </li>
+                        @endforeach
                     </ul>
-
                 </div>
             </div>
         </div>
@@ -62,9 +44,14 @@
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-notes">
                         Notes
                     </label>
+                    @error('todos')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
                     <textarea
-                        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        id="grid-notes" type="text" name="notes"></textarea>
+                        class="appearance-none h-4/5 block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 @error('notes') border-red-500 @enderror"
+                        id="grid-notes" type="text" name="notes">{{ $notes->notes }}</textarea>
                 </div>
             </div>
         </div>
@@ -73,35 +60,42 @@
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     To do
                 </label>
+                @error('notes')
+                    <div class="text-red-500 mt-2 text-sm">
+                        {{ $message }}
+                    </div>
+                @enderror
                 <textarea
-                    class="appearance-none h-4/5 block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                    id="grid-to-do" type="text" name='todos'></textarea>
+                    class="appearance-none h-4/5 block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 @error('todos') border-red-500 @enderror"
+                    id="grid-to-do" type="text" name='todos'>{{ $todos->todos }}</textarea>
             </div>
             <div class="w-full md:w-1/2">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-websites">
                     Websites
                 </label>
-                <div class="flex justify-between space-x-5 mb-5">
+                <div class="flex flex-col justify-between space-x-5 mb-5">
                     <input
-                        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        id="grid-websites" type="text">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-12">
-                        Add
-                    </button>
+                        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 @error('website') border-red-500 @enderror"
+                        id="grid-websites" type="text" name="website">
+                    @error('website')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                            Example: http://google.com
+                        </div>
+                    @enderror
                 </div>
+
                 <ul class="flex flex-col justify-between space-y-2">
-                    <li>
-                        <button
-                            class="w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                            google.com
-                        </button>
-                    </li>
-                    <li>
-                        <button
-                            class="w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                            google.com
-                        </button>
-                    </li>
+                    @foreach ($websites as $website)
+                        <li>
+                            <button
+                                class="w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                                <a href="{{ $website->website }}">
+                                    {{ $website->website }}
+                                </a>
+                            </button>
+                        </li>
+                    @endforeach
                 </ul>
             </div>
         </div>
